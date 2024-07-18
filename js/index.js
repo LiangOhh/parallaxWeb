69ccb714a4c046317f40c9dab975d1ab1b8da5c7
Liang <Liang@163.com>
2024-07-19 04:19:58 +0800

    animation

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // 监听scroll事件
 // 监听scroll事件
-const item = document.querySelector('#borderlast')
-console.log(item);
+/* const item = document.querySelector('#borderlast')
+// console.log(item);
 
 window.addEventListener('scroll', checkScrollEnd
 );
@@ -14,7 +14,7 @@ function checkScrollEnd() {
         console.log(1);
         window.removeEventListener('scroll', checkScrollEnd);
     }
-}
+} */
 
 // 时钟函数
 const hourEl = document.querySelector('.hour')
@@ -55,3 +55,50 @@ const scale = (num, in_min, in_max, out_min, out_max) => {
 setTime()
 
 setInterval(setTime, 1000)
+
+// 首张背景图片加载动画
+document.addEventListener('DOMContentLoaded', () => {
+    // 定义Intersection Observer的回调函数
+    function handleIntersection(entries, observer) {
+        // console.log('观测到');
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                // 添加动画类
+                // const animationId = entry.target.dataset.animationId;
+                // document.getElementById(animationId).classList.add('animate__fadeInBottomLeft', 'animate__slower');
+                // console.log(entry.target);
+                entry.target.style.animation = 'fadeIn 2s ease-in-out forwards'
+                // 停止观察当前元素，避免重复触发
+                observer.unobserve(entry.target);
+            }
+        });
+    }
+    // 创建Intersection Observer
+    const observer = new IntersectionObserver(handleIntersection, {
+        threshold: 0.1 // 元素进入视窗10%时触发
+    });
+    // 观察所有背景图片
+    const bgElements = document.querySelectorAll('.bg');
+    bgElements.forEach((bg, index) => {
+        // bg.dataset.animationId = `animation${index + 1}`;
+        observer.observe(bg);
+    });
+});
+
+// 给最后一个增加谢谢观看
+document.addEventListener('DOMContentLoaded', () => {
+    // 定义Intersection Observer的回调函数
+    function handleIntersection(entries, observer) {
+        let entry = entries[0]
+        if (entry.isIntersecting) {
+            // console.log(entry.target);
+            entry.target.style.animation = 'showLastText 4s linear forwards '
+            observer.unobserve(entry.target);
+        }
+    }
+    const observer_last = new IntersectionObserver(handleIntersection, {
+        threshold: 0.9
+    });
+    const lastBg = document.querySelector('#borderlast');
+    observer_last.observe(lastBg)
+});
